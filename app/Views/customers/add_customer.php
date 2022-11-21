cec339397e623a30328c5efe1720f8e1debdeebb
yungga123 <53594401+yungga123@users.noreply.github.com>
2022-11-21 21:17:27 +0800

    customers

---
+++ app/Views/customers/add_customer.php
@@ -0,0 +1,97 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Add Customer</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <section class="content">
+    <div class="container-fluid">
+      <div class="card">
+        <div class="card-body">
+
+          <div class="row">
+            <div class="col-sm-6">
+
+              <div class="card">
+                <div class="card-header">
+                  <b>Customer Details</b>
+                </div>
+
+                <div class="card-body row">
+                  <div class="col-sm-12">
+                    <div class="form-group">
+                      <label>Customer Name</label>
+                      <input type="text" class="form-control" placeholder="Enter ...">
+                    </div>
+                    <div class="form-group">
+                      <label>Contact Person</label>
+                      <input type="text" class="form-control" placeholder="Enter ...">
+                    </div>
+                    <div class="form-group">
+                      <label>Text</label>
+                      <input type="text" class="form-control" placeholder="Enter ...">
+                    </div>
+                    <div class="form-group">
+                      <label>Text</label>
+                      <input type="text" class="form-control" placeholder="Enter ...">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-header">
+                  <b>Address</b>
+                </div>
+
+                <div class="card-body row">
+                  <div class="col-sm-12">
+                    <div class="form-group">
+                      <label>Region</label>
+                      <select id="region" class="form-control" placeholder="Enter ..."></select>
+                    </div>
+                    <div class="form-group">
+                      <label>Province</label>
+                      <select id="province" class="form-control" placeholder="Enter ..."></select>
+                    </div>
+                    <div class="form-group">
+                      <label>City</label>
+                      <select id="city" class="form-control" placeholder="Enter ..."></select>
+                    </div>
+                    <div class="form-group">
+                      <label>Barangay</label>
+                      <select id="barangay" class="form-control" placeholder="Enter ..."></select>
+                    </div>
+                    <div class="form-group">
+                      <label>Text</label>
+                      <select class="form-control" placeholder="Enter ..."></select>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-footer">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+      
+
+    </div>
+  </section>
+
+</div>
