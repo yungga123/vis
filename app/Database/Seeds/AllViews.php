2c725200cec6d3a5b771bb82d21ef88a96875815
yungga123 <53594401+yungga123@users.noreply.github.com>
2022-12-21 21:22:34 +0800

    done fix

---
+++ app/Database/Seeds/AllViews.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class AllViews extends Seeder
+{
+    public function run()
+    {
+        $db = \Config\Database::connect();
+        $db->query("
+            DROP VIEW IF EXISTS
+                customer_view, employees_view, task_lead
+        ");
+        $this->call('EmployeesView');
+        $this->call('CustomerView');
+        $this->call('TaskleadView');
+    }
+}
