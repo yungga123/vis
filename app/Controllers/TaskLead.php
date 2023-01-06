ede5da575872ea2c6d9d82f950850031d3ec829f
yungga123 <53594401+yungga123@users.noreply.github.com>
2023-01-06 15:05:35 +0800

    upload file done, working with retrieve

--- app/Controllers/TaskLead.php
+++ app/Controllers/TaskLead.php
@@ -7,6 +7,7 @@ use App\Models\CustomersModel;
 use App\Models\TaskLeadModel;
 use App\Models\TaskLeadView;
 use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\Files\File;
 use CodeIgniter\I18n\Time;
 use monken\TablesIgniter;
 
@@ -16,29 +17,25 @@ class TaskLead extends BaseController
     use ResponseTrait;
     public function index()
     {
-        if (session('logged_in')==true)
-        {
+        if (session('logged_in') == true) {
             $data['title'] = 'Task/Leads Monitoring';
             $data['uri'] = service('uri');
 
-            echo view('templates/header',$data);
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('task_lead/task_lead_menu');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        else
-        {
+        } else {
             return redirect()->to('login');
         }
     }
 
     public function add_project()
     {
-        if (session('logged_in')==true)
-        {   
+        if (session('logged_in') == true) {
             $time = new Time('now');
 
             $customersModel = new CustomersModel();
@@ -47,18 +44,16 @@ class TaskLead extends BaseController
             $data['customers'] = $customersModel->findAll();
             $data['uri'] = service('uri');
             $data['date_quarter'] = $time->getQuarter();
-            
 
-            echo view('templates/header',$data);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('task_lead/add_project');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        else
-        {
+        } else {
             return redirect()->to('login');
         }
     }
@@ -116,7 +111,7 @@ class TaskLead extends BaseController
             "max_forecast_date" => $this->request->getPost('max_forecast_date'),
         ];
 
-        if (!$taskleadModel->update($id,$data)) {
+        if (!$taskleadModel->update($id, $data)) {
             $validate['messages'] = $taskleadModel->errors();
         } else {
             $validate['success'] = true;
@@ -127,131 +122,115 @@ class TaskLead extends BaseController
 
     public function project_list()
     {
-        if (session('logged_in')==true)
-        {   
+        if (session('logged_in') == true) {
             $data['title'] = 'Project List';
             $data['uri'] = service('uri');
-            
-            echo view('templates/header',$data);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('task_lead/project_list');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        else
-        {
+        } else {
             return redirect()->to('login');
         }
     }
 
     public function manager_project_list()
     {
-        
-        if (session('logged_in')==true && (session('access')=='manager' || session('access')=='admin'))
-        {   
+
+        if (session('logged_in') == true && (session('access') == 'manager' || session('access') == 'admin')) {
             $data['title'] = 'Managers Project List';
             $data['uri'] = service('uri');
-            
-            echo view('templates/header',$data);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('task_lead/project_list');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        elseif(session('logged_in')==true)
-        {
+        } elseif (session('logged_in') == true) {
             $data['title'] = 'Invalid Access';
             $data['page_title'] = 'Invalid Access';
             $data['href'] = site_url('tasklead');
             $data['uri'] = service('uri');
-            
-            echo view('templates/header',$data);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('templates/offlimits');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        else
-        {
+        } else {
             return redirect()->to('login');
         }
     }
 
     public function project_list_booked()
     {
-        if (session('logged_in')==true)
-        {   
+        if (session('logged_in') == true) {
             // $taskleadModel = new TaskLeadModel();
             $taskleadView = new TaskLeadView();
             $data['title'] = 'Booked Project List';
             $data['uri'] = service('uri');
             $data['booked_projects'] = $taskleadView->paginate(10);
             $data['pager'] = $taskleadView->pager;
-            
-            echo view('templates/header',$data);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('task_lead/project_list_booked');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        else
-        {
+        } else {
             return redirect()->to('login');
         }
     }
 
     public function manager_project_list_booked()
     {
-        
-        if (session('logged_in')==true && (session('access')=='manager' || session('access')=='admin'))
-        {   
+
+        if (session('logged_in') == true && (session('access') == 'manager' || session('access') == 'admin')) {
             $data['title'] = 'Booked Managers Project List';
             $data['uri'] = service('uri');
-            
-            echo view('templates/header',$data);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('task_lead/project_list_booked');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        elseif(session('logged_in')==true)
-        {
+        } elseif (session('logged_in') == true) {
             $data['title'] = 'Invalid Access';
             $data['page_title'] = 'Invalid Access';
             $data['href'] = site_url('tasklead');
             $data['uri'] = service('uri');
-            
-            echo view('templates/header',$data);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('templates/offlimits');
             echo view('templates/footer');
             echo view('task_lead/script');
-        }
-        else
-        {
+        } else {
             return redirect()->to('login');
         }
     }
 
     public function getProjectListManager()
-	{
-		$taskleadModel = new TaskLeadModel();
+    {
+        $taskleadModel = new TaskLeadModel();
 
         $taskleadTable = new TablesIgniter();
         $taskleadTable->setTable($taskleadModel->noticeTable())
-            ->setDefaultOrder("id","DESC")
+            ->setDefaultOrder("id", "DESC")
             ->setOrder([
                 "id",
                 null,
@@ -308,15 +287,15 @@ class TaskLead extends BaseController
                 "project_duration"
             ]);
         return $taskleadTable->getDatatable();
-	}
+    }
 
     public function getProjectList()
-	{
-		$taskleadModel = new TaskLeadModel();
+    {
+        $taskleadModel = new TaskLeadModel();
 
         $taskleadTable = new TablesIgniter();
         $taskleadTable->setTable($taskleadModel->noticeTableWhere(session('employee_id')))
-            ->setDefaultOrder("id","DESC")
+            ->setDefaultOrder("id", "DESC")
             ->setOrder([
                 "id",
                 null,
@@ -373,15 +352,15 @@ class TaskLead extends BaseController
                 "project_duration"
             ]);
         return $taskleadTable->getDatatable();
-	}
+    }
 
     public function getProjectListBookedManager()
-	{
-		$taskleadModel = new TaskLeadModel();
+    {
+        $taskleadModel = new TaskLeadModel();
 
         $taskleadTable = new TablesIgniter();
         $taskleadTable->setTable($taskleadModel->noticeTableBooked())
-            ->setDefaultOrder("id","DESC")
+            ->setDefaultOrder("id", "DESC")
             ->setOrder([
                 "id",
                 null,
@@ -438,15 +417,15 @@ class TaskLead extends BaseController
                 "project_duration"
             ]);
         return $taskleadTable->getDatatable();
-	}
+    }
 
     public function getProjectBookedList()
-	{
-		$taskleadModel = new TaskLeadModel();
+    {
+        $taskleadModel = new TaskLeadModel();
 
         $taskleadTable = new TablesIgniter();
         $taskleadTable->setTable($taskleadModel->noticeTableBookedWhere(session('employee_id')))
-            ->setDefaultOrder("id","DESC")
+            ->setDefaultOrder("id", "DESC")
             ->setOrder([
                 "id",
                 null,
@@ -503,7 +482,7 @@ class TaskLead extends BaseController
                 "project_duration"
             ]);
         return $taskleadTable->getDatatable();
-	}
+    }
 
     public function edit_project($id)
     {
@@ -512,7 +491,7 @@ class TaskLead extends BaseController
             $taskleadModel = new TaskLeadModel();
             $customersModel = new CustomersModel();
 
-            
+
             $data['title'] = 'Update a project';
             $data['page_title'] = 'Update project';
             $data['customers'] = $customersModel->findAll();
@@ -532,9 +511,10 @@ class TaskLead extends BaseController
         }
     }
 
-    public function update_project_status($id,$status) {
+    public function update_project_status($id, $status)
+    {
         if (session('logged_in') == true) {
-            
+
             $taskleadModel = new TaskLeadModel();
             $taskleadData = $taskleadModel->find($id);
             $data_model = [
@@ -546,7 +526,7 @@ class TaskLead extends BaseController
                     $status_text = "<h1 class='text-danger'>IDENTIFIED (10%)</h1>";
                     $quotation_num = "";
                     break;
-                
+
                 case '30.00':
                     $status_text = "<h1 class='text-secondary'>QUALIFIED (30%)</h1>";
                     $quotation_num = "";
@@ -555,8 +535,8 @@ class TaskLead extends BaseController
                 case '50.00':
                     $status_text = "<h1 class='text-warning'>DEVELOPED SOLUTION (50%)</h1>";
 
-                    if ($taskleadData['quotation_num']=="") {
-                        $quotation_num = "QTN".date('Ymd')."001";
+                    if ($taskleadData['quotation_num'] == "") {
+                        $quotation_num = "QTN" . date('Ymd') . "001";
                         $data_model['quotation_num'] = $quotation_num;
                     } else {
                         $quotation_num = "";
@@ -567,8 +547,8 @@ class TaskLead extends BaseController
                 case '70.00':
                     $status_text = "<h1 class='text-info'>EVALUATION (70%)</h1>";
 
-                    if ($taskleadData['quotation_num']=="") {
-                        $quotation_num = "QTN".date('Ymd')."001";
+                    if ($taskleadData['quotation_num'] == "") {
+                        $quotation_num = "QTN" . date('Ymd') . "001";
                         $data_model['quotation_num'] = $quotation_num;
                     } else {
                         $quotation_num = "";
@@ -579,15 +559,15 @@ class TaskLead extends BaseController
                 case '90.00':
                     $status_text = "<h1 class='text-primary'>NEGOTIATION (90%)</h1>";
 
-                    if ($taskleadData['quotation_num']=="") {
-                        $quotation_num = "QTN".date('Ymd')."001";
+                    if ($taskleadData['quotation_num'] == "") {
+                        $quotation_num = "QTN" . date('Ymd') . "001";
                         $data_model['quotation_num'] = $quotation_num;
                     } else {
                         $quotation_num = "";
                     }
 
                     break;
-                
+
                 default:
                     $status_text = "";
                     $quotation_num = "";
@@ -601,9 +581,9 @@ class TaskLead extends BaseController
             $data['href'] = site_url('project-list');
             $data['status'] = $status_text;
             $data['quotation_num'] = $quotation_num;
-            
 
-            $taskleadModel->update($id,$data_model);
+
+            $taskleadModel->update($id, $data_model);
 
             echo view('templates/header', $data);
             echo view('task_lead/header');
@@ -612,28 +592,27 @@ class TaskLead extends BaseController
             echo view('task_lead/update_tasklead_successpage');
             echo view('templates/footer');
             echo view('task_lead/script');
-
-            
         } else {
             return redirect()->to('login');
         }
     }
 
-    public function booked_status($id) {
+    public function booked_status($id)
+    {
         if (session('logged_in') == true) {
 
             $taskleadModel = new TaskLeadModel();
             $taskleadData = $taskleadModel->find($id);
 
-            if ($taskleadData['quotation_num']=="") {
-                $quotation_num = "QTN".date('Ymd')."001";
-                $status["quotation_num"] = $quotation_num; 
-                $taskleadModel->update($id,$status);
+            if ($taskleadData['quotation_num'] == "") {
+                $quotation_num = "QTN" . date('Ymd') . "001";
+                $status["quotation_num"] = $quotation_num;
+                $taskleadModel->update($id, $status);
             } else {
                 $quotation_num = "";
             }
 
-            
+
             $data['title'] = 'Booked Task Lead';
             $data['page_title'] = 'Book a Task Lead';
             $data['quotation_num'] = $quotation_num;
@@ -652,7 +631,8 @@ class TaskLead extends BaseController
         }
     }
 
-    public function booked_status_validate() {
+    public function booked_status_validate()
+    {
         $taskleadModel = new TaskLeadModel();
         $validate = [
             "success" => false,
@@ -667,7 +647,7 @@ class TaskLead extends BaseController
             "project_finish_date" => $this->request->getPost('project_finish_date'),
         ];
 
-        if (!$taskleadModel->update($id,$data)) {
+        if (!$taskleadModel->update($id, $data)) {
             $validate['messages'] = $taskleadModel->errors();
         } else {
             $validate['success'] = true;
@@ -676,11 +656,12 @@ class TaskLead extends BaseController
         echo json_encode($validate);
     }
 
-    public function delete_tasklead($id) {
+    public function delete_tasklead($id)
+    {
         if (session('logged_in') == true) {
 
             $taskleadModel = new TaskLeadModel();
-            
+
             $data['title'] = 'Delete Task/Lead';
             $data['page_title'] = 'Delete Task/Lead';
             $data['uri'] = service('uri');
@@ -699,24 +680,87 @@ class TaskLead extends BaseController
         }
     }
 
-    public function project_booked_details($id) {
-        if (session('logged_in')==true)
-        {
+    public function project_booked_details($id)
+    {
+        if (session('logged_in') == true) {
+            helper('filesystem');
             $taskleadView = new TaskLeadView();
             $data['title'] = 'Project Booked Details';
             $data['uri'] = service('uri');
             $data['project_detail'] = $taskleadView->find($id);
+            $data['errors'] = [];
 
-            echo view('templates/header',$data);
+            $path = WRITEPATH . 'uploads/' . $id;
+
+            $data['map'] = directory_map($path);
+
+            echo view('templates/header', $data);
             echo view('task_lead/header');
             echo view('templates/navbar');
             echo view('templates/sidebar');
             echo view('task_lead/project_booked_details');
             echo view('templates/footer');
             echo view('task_lead/script');
+        } else {
+            return redirect()->to('login');
         }
-        else
-        {
+    }
+
+    public function upload($id)
+    {
+        if (session('logged_in') == true) {
+            $validationRule = [
+                'project_file' => [
+                    'label' => 'File',
+                    'rules' => 'uploaded[project_file]'
+                        . '|max_size[project_file,5000]'
+                        . '|ext_in[project_file,xlsx,jpg,jpeg,csv,docx,pdf]'
+                ],
+            ];
+            if (!$this->validate($validationRule)) {
+                helper('filesystem');
+                $data = ['errors' => $this->validator->getErrors()];
+                
+                $taskleadView = new TaskLeadView();
+                $data['title'] = 'Project Booked Details';
+                $data['uri'] = service('uri');
+                $data['project_detail'] = $taskleadView->find($id);
+
+                $path = WRITEPATH . 'uploads/' . $id;
+
+                $data['map'] = directory_map($path);
+
+                return view('templates/header', $data)
+                    .view('task_lead/header')
+                    .view('templates/navbar')
+                    .view('templates/sidebar')
+                    .view('task_lead/project_booked_details')
+                    .view('templates/footer')
+                    .view('task_lead/script');
+            }
+
+            $img = $this->request->getFile('project_file');
+
+            if (!$img->hasMoved()) {
+                $filepath = WRITEPATH . 'uploads/project-booked/' . $img->store($id,$img->getClientName());
+
+                $data = ['uploaded_flleinfo' => new File($filepath)];
+                $data['title'] = 'Upload File Success';
+                $data['page_title'] = 'File upload success!';
+                $data['href'] = site_url('project-list-booked');
+                $data['uri'] = service('uri');
+
+                 return view('templates/header', $data)
+                 .view('task_lead/header')
+                 .view('templates/navbar')
+                 .view('templates/sidebar')
+                 .view('templates/file-successpage')
+                 .view('templates/footer')
+                 .view('task_lead/script');
+            }
+
+
+        } else {
             return redirect()->to('login');
         }
     }
