ccb7814f3708265bc840cb0d7c34b20cd23723a4
radyballs69 <geraldsedenio1@gmail.com>
2023-01-25 22:16:13 +0800

    Created a filter for checking whether user is logged in or not. To act as middleware. And changes in routes - add the said filters.

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -8,6 +8,8 @@ use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
+use App\Filters\CheckAuthFilter;
+use App\Filters\NotLoggedFilter;
 
 class Filters extends BaseConfig
 {
@@ -23,6 +25,8 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'checkauth'     => CheckAuthFilter::class,
+        'notlogged'     => NotLoggedFilter::class,
     ];
 
     /**
