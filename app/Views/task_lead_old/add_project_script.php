04600b29037aada7dbe7d103e33e90f6d166a818
radyballs69 <geraldsedenio1@gmail.com>
2023-03-06 23:31:37 +0800

    Deleted old and unused files

--- app/Views/task_lead_old/add_project_script.php
+++
@@ -1,5 +0,0 @@
-<script>
-    $('#customer_id option').attr('disabled',true);
-
-    $('#customer_id option[value="<?= $selectedCustomer ?>"]').attr('disabled',false).attr('selected',true);
-</script>
