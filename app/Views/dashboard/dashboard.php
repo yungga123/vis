966422eee9644fcb886ff65480ffd596e3021beb
Rady <geraldsedenio1@gmail.com>
2023-07-28 09:40:16 +0800

    Dashboard changes in codes Added the schedules for the current date

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -1,16 +1,59 @@
 <?= $this->extend('templates/default'); ?>
 <?= $this->section('content'); ?>
 	<div class="container-fluid mb-3">
-		<?php if (is_array($modules)): ?>
-			<div class="row">
-				<?php
-					echo $modules['hr_modules'];
-					echo $modules['clients_modules'];
-					echo $modules['sales_modules'];
-					echo $modules['inventory_modules'];
-					echo $modules['settings_modules'];
-				?>
+		<div class="row">
+			<div class="col-12">
+				<div class="card card-outline card-info">
+					<div class="card-header">
+						<h3 class="card-title">Schedules Today</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+					</div>
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-3 col-sm-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="card-title">Legend</h3>
+									</div>
+									<div class="card-body">
+										<?= $type_legend ?>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-9 col-sm-12">                    
+								<div class="card">
+									<div class="card-body">
+										<?= $schedules ?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		<?php else: echo $modules; endif; ?>
+			<div class="col-12">
+				<div class="card card-outline card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">Your Modules</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">						
+						<div class="row">
+							<?php
+								echo $modules;
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 <?= $this->endSection(); ?>
