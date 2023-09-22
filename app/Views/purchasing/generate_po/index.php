a4d16969c2ff6201576c5cd04f4ec59ff3262eb2
yungga123 <53594401+yungga123@users.noreply.github.com>
2023-09-22 15:11:59 +0800

    WORKING WITH PO GENERATE

---
+++ app/Views/purchasing/generate_po/index.php
@@ -0,0 +1,20 @@
+<?=$this->extend('templates/default');?>
+<?=$this->section('content');?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <table id="po_table" class="table table-hover table-striped nowrap" width="100%">
+                        <thead class="nowrap">
+                            <tr> 
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?=$this->endSection();?>
