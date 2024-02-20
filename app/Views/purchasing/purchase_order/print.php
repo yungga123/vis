34edc97de3f402fe035ac6b9c293284c461d1d91
Gerald Sedenio <61409307+radyballs69@users.noreply.github.com>
2024-02-20 14:16:54 +0800

    PO and PRF Changes (#91)
    * PO and PRF changes:
    Purchase Order
    - Print PO: changed PO number and date to approved date (not current date)
    
    PRF
    - Print PRF: added item model in the list
    
    * PO print minor changed.

--- app/Views/purchasing/purchase_order/print.php
+++ app/Views/purchasing/purchase_order/print.php
@@ -7,6 +7,7 @@ $total_amount		= 0;
 $net_of_vat_amount	= 0;
 $vat_amount			= 0;
 $with_vat 			= $purchase_order['with_vat'] != '0';
+$approved_at 		= $purchase_order['approved_at'];
 ?>
 <div class="container-fluid">
 	<div class="row">		
@@ -23,7 +24,15 @@ $with_vat 			= $purchase_order['with_vat'] != '0';
                             <br>
 							<span class="text-danger">
 								<?php 
-									$code_format 	= "{$form_code}-{$purchase_order['supplier_id']}-". date('y') .'-'. date('md') .'-'. $purchase_order['id'];
+									$year 			= date('y');
+									$month_day 		= date('md');
+
+									if (! empty($approved_at)) {
+										$year 		= format_date($approved_at, 'y');
+										$month_day	= format_date($approved_at, 'md');
+									}
+
+									$code_format 	= "{$form_code}-{$purchase_order['supplier_id']}-{$year}-{$month_day}-{$purchase_order['id']}";
 									echo $code_format;
 								?>
 							</span>
@@ -33,7 +42,7 @@ $with_vat 			= $purchase_order['with_vat'] != '0';
                         <td>
 							<span class="text-bold">PURCHASE ORDER DATE</span>
                             <br>
-                            <?php echo date('F d, Y'); ?>
+                            <?= $approved_at ? format_date($approved_at, 'F d, Y') : 'N/A' ?>
                         </td>
                     </tr>
                     <br>
@@ -124,9 +133,9 @@ $with_vat 			= $purchase_order['with_vat'] != '0';
 								<td class="text-center"><?= $val['brand'] ?? 'N/A' ?></td>
 								<td class="text-center"><?= $val['item_description'] ?? 'N/A' ?></td>
 								<td class="text-center"><?= $val['size'] ?? 'N/A' ?></td>
-								<td class="text-center"><?= ($val['unit'] ?? 'N/A') ?></td>
+								<td class="text-center"><?= $val['unit'] ?? 'N/A' ?></td>
 								<td class="text-center"><?= $val['quantity_in'] ?? 'N/A' ?></td>
-								<td class="text-right"><?= $val['item_sdp'] ?? 'N/A' ?></td>
+								<td class="text-right"><?= number_format($val['item_sdp'] ?? 0) ?></td>
 								<td class="text-right"><?= number_format($discount, 2) ?></td>
 								<td class="text-right"><?= number_format($total_price, 2) ?></td>
 								<td class="text-center"><?= $rpf['date_needed'] ?? 'N/A' ?></td>
@@ -152,7 +161,7 @@ $with_vat 			= $purchase_order['with_vat'] != '0';
 							<div>NET OF VAT</div>
 							<div>PLUS 12% VAT</div>
                         </td>
-                        <td class="text-right">
+                        <td class="text-right" colspan="2">
 							<div>PHP <?= number_format($sub_total, 2) ?></div>
 							<div id="net_of_vat_amount">
 								<?= number_format($with_vat ? $net_of_vat_amount : 0, 2) ?>
@@ -161,13 +170,12 @@ $with_vat 			= $purchase_order['with_vat'] != '0';
 								<?= number_format($with_vat ? $vat_amount : 0, 2) ?>
 							</div>
                         </td>
-						<td rowspan="2"></td>
                     </tr>
                     <tr>
                         <td class="text-bold text-right" colspan="2">
 							TOTAL AMOUNT<span id="vat_text"></span>
 						</td>
-                        <td class="text-bold text-right">
+                        <td class="text-bold text-right" colspan="2">
 							PHP <span id="total_amount"><?= number_format($total_amount, 2) ?></span>
 						</td>
                     </tr>
