603fbf6a0dc001f6cbfec2a918656cf73ae4206a
Rady <geraldsedenio1@gmail.com>
2024-10-06 14:51:46 +0800

    Mail Config - label updates

--- app/Views/settings/mail_config/mail_list.php
+++ app/Views/settings/mail_config/mail_list.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-12">                    
+    <div class="col-12">
         <div class="card">
             <div class="card-header d-flex flex-column">
                 <h4 class="card-title text-bold mr-3">List of Modules with Mail Notification</h4>
@@ -12,8 +12,8 @@
             </div>
             <div class="card-body">
                 <p>
-                    <strong>Note:</strong> 
-                    In <strong>CC Recipients</strong>, if multiple emails - separate it by comma (e.g. mail@email.com, mail2@email.com). <br>
+                    <strong>Note:</strong>
+                    In <strong>Email Received</strong>, if multiple emails - separate it by comma (e.g. mail@email.com, mail2@email.com). <br>
                 </p>
                 <div class="table-responsive">
                     <table id="mail_list_table" class="table table-hover table-striped nowrap align-items-middle" width="100%">
@@ -21,10 +21,10 @@
                             <tr>
                                 <th>Module Name</th>
                                 <?php if (is_developer()): ?>
-                                    <th>Has Mail Notif?</th>
-                                    <th>Is Mail Notif Enabled?</th>
+                                    <th>Sent Email Notification?</th>
+                                    <th>Enable Email Notification?</th>
                                 <?php endif; ?>
-                                <th width="50%">CC Recipients</th>
+                                <th width="50%">Email Received</th>
                                 <th class="text-center">Save</th>
                             </tr>
                         </thead>
@@ -32,15 +32,15 @@
                             <?php if (is_developer()): ?>
                                 <?php foreach ($modules as $key => $val): ?>
                                     <?php
-                                        $_mail_notifs   = check_param($mail_notifs, $key, '', true);
-                                        $has_mail_notif = check_param($_mail_notifs, 'has_mail_notif');
-                                        $has_mail_notif = $has_mail_notif ? 1 : 0;
-                                        $is_enabled     = check_param($_mail_notifs, 'is_mail_notif_enabled');
-                                        $is_enabled     = $is_enabled ? 1 : 0;
-                                        $cc_recipients  = check_param($_mail_notifs, 'cc_recipients', '', true);
-                                        $data           = 'data-module_code="'.$key.'"';
-                                        $data2          = $data . ' data-has_mail_notif="'.$has_mail_notif.'"';
-                                        $params         = "'{$key}', '{$has_mail_notif}', '{$is_enabled}'";
+                                    $_mail_notifs   = check_param($mail_notifs, $key, '', true);
+                                    $has_mail_notif = check_param($_mail_notifs, 'has_mail_notif');
+                                    $has_mail_notif = $has_mail_notif ? 1 : 0;
+                                    $is_enabled     = check_param($_mail_notifs, 'is_mail_notif_enabled');
+                                    $is_enabled     = $is_enabled ? 1 : 0;
+                                    $cc_recipients  = check_param($_mail_notifs, 'cc_recipients', '', true);
+                                    $data           = 'data-module_code="' . $key . '"';
+                                    $data2          = $data . ' data-has_mail_notif="' . $has_mail_notif . '"';
+                                    $params         = "'{$key}', '{$has_mail_notif}', '{$is_enabled}'";
                                     ?>
                                     <tr id="ROW_<?= $key ?>">
                                         <td><?= $val ?></td>
@@ -59,10 +59,10 @@
                                         </td>
                                     </tr>
                                 <?php endforeach; ?>
-                            <?php else: ?> 
+                            <?php else: ?>
                                 <?php foreach ($mail_notifs as $key => $val): ?>
                                     <?php
-                                        $params = "'{$key}', '{$val['has_mail_notif']}', '{$val['is_mail_notif_enabled']}'";
+                                    $params = "'{$key}', '{$val['has_mail_notif']}', '{$val['is_mail_notif_enabled']}'";
                                     ?>
                                     <tr id="ROW_<?= $key ?>">
                                         <td><?= get_modules($key) ?></td>
